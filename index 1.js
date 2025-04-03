0f928be281b7e692944187ba5c6089fa4bee583b
Harlitoo <h.harley85@gmail.com>
2025-04-03 13:44:24 -0300

    Add files via upload

---
+++ index 1.js
@@ -0,0 +1,14 @@
+import mysql from 'mysql2';
+
+const pool = mysql.createPool({
+ host: '127.0.0.1',
+ user: 'root',
+ password: 'senac@02',
+ database: 'sakila'
+
+
+
+}).promise();
+
+const result = await pool.query("select * from `sakila`.`address`,`sakila`.`city`;");
+console.log(result);
